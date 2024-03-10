7a3206343959fefd2ab8bd25a9a74b02e92b4831
aya-weblike <aya.weblike@gmail.com>
2024-03-10 15:17:06 +0900

    js追加

--- js/script.js
+++ js/script.js
@@ -3,16 +3,83 @@
 -------------------------------------------*/
 const btn = document.querySelector('.btn-menu');
 const nav = document.querySelector('.header-nav');
+const navItems = document.querySelectorAll('nav li');
+const navOptions = {
+	duration:1400,
+	easing:'ease',
+	fill:'forwards',
+};
+
 
 btn.addEventListener('click' , () => {
 	nav.classList.toggle('open-menu');
 	btn.classList.toggle('close-menu');
+
+	if(nav.classList.contains('open-menu') == true )
+	{
+		// メニューリンクを１つずつ順に表示
+		navItems.forEach((navItem,index)=>{
+			navItem.animate(
+				{
+					opacity:[0,1],
+					translate:['2rem',0],
+				},
+				{
+					duration:2400,
+					delay:300*index,
+					easing:'ease',
+					fill:'forwards',
+				}
+			);
+		});
+	}else{
+		navItems.forEach((navItem) => {
+			navItem.animate({opacity:[1,0]},navOptions);
+		})
+	};
 });
+
+/*-------------------------------------------
+スクロールしたら、ふわっと表示
+-------------------------------------------*/
+// 監視対象が範囲内に現れたら実行する動作
+const animateFade = (entries,obs) => {
+	entries.forEach((entry) => {
+		if(entry.isIntersecting){
+			entry.target.animate(
+				{
+					opacity:[0,1],
+					filter:['blur(.4rem)','blur(0)'],
+					translate:['0 4rem',0],
+				},
+				{
+					duration:2000,
+					easing:'ease',
+					fill:'forwards',
+				}
+			);
+			// １度ふわっとさせたら監視をやめて動きをとめる
+			obs.unobserve(entry.target);
+		}
+	});
+};
+
+// 監視設定
+const fadeObserver = new IntersectionObserver(animateFade);
+
+// .fadeinを監視するよう指示
+const fadeElements = document.querySelectorAll('.fadein');
+fadeElements.forEach((fadeElement) => {
+	fadeObserver.observe(fadeElement);
+});
+
 
 
 /*-------------------------------------------
 MENU PAGE
 -------------------------------------------*/
+
+// メニューを表示させる
 const menu = document.querySelector('#menu');
 
 const menuLists = [
@@ -61,11 +128,24 @@ const menuLists = [
 for(let i=0; i < menuLists.length; i++){
 	const {name,img,price} = menuLists[i];
 	const content = `<div>
-						<img src="img/${img}" alt="">
+						<img class="menu-item" src="img/${img}" alt="">
 						<h2>${name}</h2>
 						<p>${price}円</p>
 					</div>`;
 	menu.insertAdjacentHTML('beforeend',content);
 }
 
+// 画像をぼかし→くっきりにする
+const menuItems = document.querySelectorAll('.menu-item');
 
+for(let i=0; i < menuItems.length; i++){
+	const keyframes = {
+		filter:['blue(20px)','blur(0)'],
+	};
+	const options = {
+		duration:600,
+		delay:i*300,
+		fill:'forwards',
+	};
+	menuItems[i].animate(keyframes,options);
+}
