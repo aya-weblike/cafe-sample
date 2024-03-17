afed0d35093c8bb8b95bf0d7b32a1e06e18cc0de
aya-weblike <aya.weblike@gmail.com>
2024-03-17 11:04:35 +0900

    下層ページ完成

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*-------------------------------------------
-ハンバーガーメニュー
+共通
 -------------------------------------------*/
+// ハンバーガーメニュー
 const btn = document.querySelector('.btn-menu');
 const nav = document.querySelector('.header-nav');
 const navItems = document.querySelectorAll('nav li');
@@ -9,7 +10,6 @@ const navOptions = {
 	easing:'ease',
 	fill:'forwards',
 };
-
 
 btn.addEventListener('click' , () => {
 	nav.classList.toggle('open-menu');
@@ -39,9 +39,7 @@ btn.addEventListener('click' , () => {
 	};
 });
 
-/*-------------------------------------------
-スクロールしたら、ふわっと表示
--------------------------------------------*/
+// スクロールしたら、ふわっと表示
 // 監視対象が範囲内に現れたら実行する動作
 const animateFade = (entries,obs) => {
 	entries.forEach((entry) => {
@@ -130,15 +128,15 @@ if(document.URL.match(/menu/)){
 	for(let i=0; i < menuLists.length; i++){
 		const {name,img,price} = menuLists[i];
 		const content = `<div>
-							<img class="menu-item" src="img/${img}" alt="">
-							<h2>${name}</h2>
+							<img class='product-item' src="img/menu/${img}">
+							<h2 class="product-name">${name}</h2>
 							<p>${price}円</p>
 						</div>`;
 		menu.insertAdjacentHTML('beforeend',content);
 	}
 
 	// 画像をぼかし→くっきりにする
-	const menuItems = document.querySelectorAll('.menu-item');
+	const menuItems = document.querySelectorAll('.product-item');
 
 	for(let i=0; i < menuItems.length; i++){
 		const keyframes = {
@@ -165,3 +163,79 @@ if(document.URL.match(/access/)){
 		});
 	});
 };
+
+
+/*-------------------------------------------
+GOODS PAGE
+-------------------------------------------*/
+if(document.URL.match(/goods/)){
+	// ドメイン以下のパス名が /menu.html の場合に実行する処理
+
+	// メニューを表示させる
+	const goods = document.querySelector('#goods');
+
+	const goodsLists = [
+		{
+			name:'ブレンドコーヒー豆',
+			img:'brend-roast.jpg',
+			price:500,
+		},
+		{
+			name:'深煎り豆',
+			img:'dark-roast.jpg',
+			price:500,
+		},
+		{
+			name:'浅煎り豆',
+			img:'light-roast.jpg',
+			price:500,
+		},
+		{
+			name:'エコバック',
+			img:'ecobag.jpg',
+			price:500,
+		},
+		{
+			name:'オリジナルハンカチ',
+			img:'handkerchief.jpg',
+			price:500,
+		},
+		{
+			name:'オリジナルマグ',
+			img:'mugs.jpg',
+			price:500,
+		},
+		{
+			name:'オリジナルタンブラー',
+			img:'tumbler.jpg',
+			price:500,
+		},
+	];
+
+	for(let i=0; i < goodsLists.length; i++){
+		const {name,img,price} = goodsLists[i];
+		const content = `<div>
+							<img class='product-item' src="img/goods/${img}">
+							<h2 class="product-name">${name}</h2>
+							<p>${price}円</p>
+						</div>`;
+		goods.insertAdjacentHTML('beforeend',content);
+	}
+
+	// 画像をぼかし→くっきりにする
+	const goodsItems = document.querySelectorAll('.product-item');
+
+	for(let i=0; i < goodsItems.length; i++){
+		const keyframes = {
+			filter:['blur(20px)','blur(0)'],
+		};
+		const options = {
+			duration:600,
+			delay:i*300,
+			fill:'forwards',
+		};
+		goodsItems[i].animate(keyframes,options);
+	}
+};
+
+
